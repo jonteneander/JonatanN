918bae3e13c76f6f7da022a78bc660ed5acc4016
JonteNeander <jonte.neander@gmail.com>
2018-04-05 14:38:14 +0100

    added listRentedMovies view

--- queries.sql
+++ queries.sql
@@ -6,6 +6,10 @@ SELECT * FROM listallmovies;
 
 SELECT * FROM listmoviesbygenre;
 
+-- Fråga 3: Vilka filmer som är uthyrda, vem som hyrde dem (kund) och vem som hyrde ut dem (anställd).
+
+SELECT * FROM listrentedmovies;
+
 
 
 
